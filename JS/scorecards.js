4073f68a6d0b28202be038436c37fb21349a0eff
Ishaan Mathur <ishaanmathur.16@gmail.com>
2019-01-08 20:16:31 -0600

    H

--- JS/scorecards.js
+++ JS/scorecards.js
@@ -188,12 +188,20 @@ function changeHTML() {
 
     var count = 0;
     if (num % 8 != 0) {
+        var remainder = Number(num / 8) + 1;
+        var real = (remainder - num) / 2 + 1; 
+    
+        name1[name1.length - real].style.bottom = "-4%";
+        name2[name2.length - real].style.bottom = "-4%";
+        record1[record1.length - real].style.marginTop = "3%";
+        record2[record2.length - real].style.marginTop = "3%";
+        card[card.length - real].style.height = "40%";
 
-        name1[name1.length - 3].style.bottom = "-4%";
-        name2[name2.length - 3].style.bottom = "-4%";
-        record1[record1.length - 3].style.marginTop = "3%";
-        record2[record2.length - 3].style.marginTop = "3%";
-        card[card.length - 3].style.height = "40%";
+        name1[name1.length - real - 1].style.bottom = "-4%";
+        name2[name2.length - real - 1].style.bottom = "-4%";
+        record1[record1.length - real - 1].style.marginTop = "3%";
+        record2[record2.length - real - 1].style.marginTop = "3%";
+        card[card.length - real - 1].style.height = "40%";
 
         row[Math.floor(num / 8)].style.height = "891px";
     };
