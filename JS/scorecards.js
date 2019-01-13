e8a7a761122de151b6a28a407ed447aa6985e387
Ishaan Mathur <ishaanmathur.16@gmail.com>
2019-01-12 18:22:39 -0600

    h

--- JS/scorecards.js
+++ JS/scorecards.js
@@ -176,7 +176,7 @@ function changeHTML() {
         for (var i = card.length - 1; i >= 0; i--) {
             if (card[i].style.display != "none") {
                 card[i].style.marginLeft = "29.5%";
-                row[Math.floor(i / 2)].style.height = screen.height + "px";
+                row[Math.floor(i / 4)].style.height = screen.height + "px";
                 break;
             };
         }; 
